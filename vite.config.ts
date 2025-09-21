81c0b95b3499fc20d91c9e151e0a8987dfaabbfa
artyem.stepanov <artemstepanov0114@gmail.com>
2025-09-21 12:24:30 +0700

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import {fileURLToPath, URL} from 'node:url'
+
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    outDir: 'build',
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      }
+    }
+  },
+  server: {
+    port: 3000
+  },
+})
